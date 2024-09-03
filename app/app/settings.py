8a62fd0a92d0fa005a8fb581fc523a041ca900c8
brian <brianotina20@gmail.com>
2024-09-04 01:56:18 +0300

    Created users tests

--- app/app/settings.py
+++ app/app/settings.py
@@ -21,7 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$)*ak7lhv0q#b$^&ds56-g7(o23g_l=bjg5^68ty&qsv3y6ybj'
+SECRET_KEY = (
+    'django-insecure-$)*ak7lhv0q#b$^&ds56-g7(o23g_l=bjg5^68ty&qsv3y6ybj')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True  # : W293
@@ -91,16 +92,16 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',  # noqa: E501
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',    # noqa: E501
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',    # noqa: E501
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',     # noqa: E501
     },
 ]
 
@@ -126,3 +127,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'core.User'
---
+++ app/core/tests/test_models.py
@@ -0,0 +1,35 @@
+''''Tests for models # noqa: E501'''
+
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+
+class ModelTests(TestCase):
+
+    def test_create_user_with_email_success(self):
+        email = "test@example.com"
+        password = "password123"
+        user = get_user_model().objects.create_user(email=email,
+                                                    password=password)
+
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        data = [
+            ['test1@example.com', 'test1@example.com'],
+            ['Test2@example.com', 'Test2@example.com']
+        ]
+        for email, expected in data:
+            user = get_user_model().objects.create_user(email, "password123")
+            self.assertEqual(user.email, expected)
+
+    def test_new_user_without_email_raises_error(self):
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(None, "password123")
+
+    def test_create_superuser(self):
+        user = get_user_model().objects.create_superuser('test@example.com', 'test123')   # noqa: E501
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
